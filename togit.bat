9b46545df25ab1e99e420c726f97f287d9db84ea
J B <you@example.com>
2021-04-21 17:54:48 +0300

    first commit

---
+++ togit.bat
@@ -0,0 +1,7 @@
+echo "# hw2" >> README.md
+git init
+git add --all
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Yuvbryleva/hw2.git
+git push -u origin main
